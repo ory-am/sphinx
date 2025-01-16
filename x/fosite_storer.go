canonical/hydra

feat: add consent handler for accepting a user_code

x/fosite_storer.go
@@ -12,6 +12,7 @@ import (
 	"github.com/ory/fosite/handler/openid"
 	"github.com/ory/fosite/handler/pkce"
 	"github.com/ory/fosite/handler/rfc7523"
+	"github.com/ory/fosite/handler/rfc8628"
 	"github.com/ory/fosite/handler/verifiable"
 )
 
@@ -21,6 +22,7 @@ type FositeStorer interface {
 	openid.OpenIDConnectRequestStorage
 	pkce.PKCERequestStorage
 	rfc7523.RFC7523KeyStorage
+	rfc8628.RFC8628CoreStorage
 	verifiable.NonceManager
 	oauth2.ResourceOwnerPasswordCredentialsGrantStorage
 
@@ -44,4 +46,7 @@ type FositeStorer interface {
 	// DeleteOpenIDConnectSession deletes an OpenID Connect session.
 	// This is duplicated from Ory Fosite to help against deprecation linting errors.
 	DeleteOpenIDConnectSession(ctx context.Context, authorizeCode string) error
+
+	UpdateDeviceCodeSessionByRequestID(ctx context.Context, requestID string, requester fosite.Requester) error
+	UpdateAndInvalidateUserCodeSession(ctx context.Context, signature, challenge_id string) (err error)
 }

ory-am/sphinx

x/fosite_storer.go
@@ -12,6 +12,7 @@ import (
 	"github.com/ory/fosite/handler/openid"
 	"github.com/ory/fosite/handler/pkce"
 	"github.com/ory/fosite/handler/rfc7523"
+	"github.com/ory/fosite/handler/rfc8628"
 	"github.com/ory/fosite/handler/verifiable"
 )
 
@@ -22,6 +23,7 @@ type FositeStorer interface {
 	openid.OpenIDConnectRequestStorage
 	pkce.PKCERequestStorage
 	rfc7523.RFC7523KeyStorage
+	rfc8628.RFC8628CoreStorage
 	verifiable.NonceManager
 	oauth2.ResourceOwnerPasswordCredentialsGrantStorage
 
@@ -41,4 +43,7 @@ type FositeStorer interface {
 	// DeleteOpenIDConnectSession deletes an OpenID Connect session.
 	// This is duplicated from Ory Fosite to help against deprecation linting errors.
 	DeleteOpenIDConnectSession(ctx context.Context, authorizeCode string) error
+
+	UpdateDeviceCodeSessionByRequestID(ctx context.Context, requestID string, requester fosite.Requester) error
+	UpdateAndInvalidateUserCodeSessionByRequestID(ctx context.Context, signature, request_id string) (err error)
 }